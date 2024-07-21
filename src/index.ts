bb4f48d16c36ae18df8aaf674f50a363d8d7c1a5
Kéwan B <mail@kewan.fr>
2024-07-21 15:15:38 +0200

    tests

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,12 @@
 import startHomeassitantMQTTService from './homeassist-mqtt'
 import config from './config.json'
 
-startHomeassitantMQTTService(config.mqttUrl, config.user, config.password)
+startHomeassitantMQTTService(config.mqttUrl);
+// startHomeassitantMQTTService(config.mqttUrl, config.user, config.password)
+
+// import { UltimatronBattery, BatteryState } from "./battery";
+// const mqtt = require('mqtt');
+
+// const logBattery = (battery: any, state: any) => {
+//   console.log(battery, state);
+// };
